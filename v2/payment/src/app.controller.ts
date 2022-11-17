efe769edf0bfa0d96be2361a5310998775a468a6
Artem Vlasenko <s18b3_vlasenko@179.ru>
2022-11-17 23:53:02 +0300

    copy base version from previous lab

---
+++ v2/payment/src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Payment } from './payment';
+import { AppService } from './app.service';
+
+@Controller('payment')
+export class AppController {
+  constructor(private readonly service: AppService) {}
+
+  @Post('/')
+  async createPayment(@Body() body: Payment) {
+    return await this.service.createPayment(body);
+  }
+
+  @Patch('/:paymentId')
+  async updatePayment(@Body() body: Payment, @Param('paymentId') uid: string) {
+    return await this.service.updatePaymentStatus(uid, body.status);
+  }
+
+  @Get('/:paymentId')
+  async getPayment(@Param('paymentId') uid: string) {
+    return await this.service.getPaymentByUid(uid);
+  }
+}
